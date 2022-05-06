8d25b98ae57425170f542d73fd66631c882dff54
syukatsu0415 <104782594+syukatsu0415@users.noreply.github.com>
2022-05-06 11:43:16 +0900

    Update BanDisp.vue

--- src/components/BanDisp.vue
+++ src/components/BanDisp.vue
@@ -2,6 +2,6 @@
   <div>
     <h1>画面遷移エラー</h1>
     <h2>ブラウザバック・リロードは使用しないでください。</h2>
-    <button type="“button”" onclick="location.href='http://sgholic0415.click:3000/'"> 戻る</button>
+    <button type="“button”" onclick="location.href='http://chat.syukatsu0415.com:3000/'"> 戻る</button>
   </div>
 </template>
